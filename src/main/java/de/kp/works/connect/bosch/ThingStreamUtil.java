eb85a3b3877d3e0191e0637ba0bfe3d010974ed4
Predictive Works <info@predictiveworks.eu>
2020-04-09 15:20:38 +0200

    Thing connector updated

--- src/main/java/de/kp/works/connect/bosch/ThingStreamUtil.java
+++ src/main/java/de/kp/works/connect/bosch/ThingStreamUtil.java
@@ -22,8 +22,11 @@ import org.apache.spark.streaming.api.java.JavaDStream;
 import org.apache.spark.streaming.ws.*;
 
 import co.cask.cdap.api.data.format.StructuredRecord;
+import co.cask.cdap.api.data.schema.Schema;
 import co.cask.cdap.etl.api.streaming.StreamingContext;
 
+import java.util.ArrayList;
+import java.util.List;
 import java.util.Properties;
 
 import org.apache.spark.api.java.JavaRDD;
@@ -31,27 +34,144 @@ import org.apache.spark.api.java.function.Function;
 import org.apache.spark.storage.StorageLevel;
 
 public class ThingStreamUtil {
-	
+
 	static JavaDStream<StructuredRecord> getStructuredRecordJavaDStream(StreamingContext context, ThingConfig config) {
-		
-		Properties properties = null;
-		StorageLevel storageLevel = null;
-		
-		return DittoStreamUtils.createDirectStream(
-				context.getSparkStreamingContext(), properties, storageLevel).transform(new RecordTransform());
-		
+
+		Properties properties = config.getThingConf();
+		StorageLevel storageLevel = StorageLevel.MEMORY_ONLY_SER();
+
+		return DittoStreamUtils.createDirectStream(context.getSparkStreamingContext(), properties, storageLevel)
+				.transform(new RecordTransform(properties));
+
 	}
-	
+
 	public static class RecordTransform implements Function<JavaRDD<String>, JavaRDD<StructuredRecord>> {
 
 		private static final long serialVersionUID = -4426828579132235453L;
 
+		private Properties properties;
+
+		public RecordTransform(Properties properties) {
+			this.properties = properties;
+		}
+
 		@Override
-		public JavaRDD<StructuredRecord> call(JavaRDD<String> v1) throws Exception {
-			// TODO Auto-generated method stub
+		public JavaRDD<StructuredRecord> call(JavaRDD<String> input) throws Exception {
+
+			if (input.isEmpty())
+				return input.map(new EmptyFunction());
+
+			/*
+			 * Transform into structured records depends on the user-specified indicators
+			 */
+			if (properties.containsKey(DittoUtils.DITTO_THING_CHANGES())) {
+
+				String flag = properties.getProperty(DittoUtils.DITTO_THING_CHANGES());
+				if (flag.equals("true")) {
+					return input.map(new ThingFunction());
+				}
+
+			}
+
+			if (properties.containsKey(DittoUtils.DITTO_FEATURES_CHANGES())) {
+
+				String flag = properties.getProperty(DittoUtils.DITTO_FEATURES_CHANGES());
+				if (flag.equals("true")) {
+					return input.map(new FeaturesFunction());
+				}
+
+			}
+
+			if (properties.containsKey(DittoUtils.DITTO_FEATURE_CHANGES())) {
+
+				String flag = properties.getProperty(DittoUtils.DITTO_FEATURE_CHANGES());
+				if (flag.equals("true")) {
+					return input.map(new FeatureFunction());
+				}
+
+			}
+
+			if (properties.containsKey(DittoUtils.DITTO_LIVE_MESSAGES())) {
+
+				String flag = properties.getProperty(DittoUtils.DITTO_LIVE_MESSAGES());
+				if (flag.equals("true")) {
+					return input.map(new MessageFunction());
+				}
+
+			}
+
+			return input.map(new EmptyFunction());
+
+		}
+
+	}
+
+	private static class ThingFunction implements Function<String, StructuredRecord> {
+
+		private static final long serialVersionUID = -4241854791385529970L;
+
+		@Override
+		public StructuredRecord call(String in) throws Exception {
+			// TODO
 			return null;
 		}
-		
+
+	}
+
+	private static class FeaturesFunction implements Function<String, StructuredRecord> {
+
+		private static final long serialVersionUID = -7865686438932012000L;
+
+		@Override
+		public StructuredRecord call(String in) throws Exception {
+			// TODO
+			return null;
+		}
+
+	}
+
+	private static class FeatureFunction implements Function<String, StructuredRecord> {
+
+		private static final long serialVersionUID = 8855558414425001019L;
+
+		@Override
+		public StructuredRecord call(String in) throws Exception {
+			// TODO
+			return null;
+		}
+
+	}
+
+	private static class MessageFunction implements Function<String, StructuredRecord> {
+
+		/**
+		 * 
+		 */
+		private static final long serialVersionUID = 1L;
+
+		@Override
+		public StructuredRecord call(String in) throws Exception {
+			// TODO
+			return null;
+		}
+
+	}
+
+	private static class EmptyFunction implements Function<String, StructuredRecord> {
+
+		private static final long serialVersionUID = -2582275414113323812L;
+
+		@Override
+		public StructuredRecord call(String in) throws Exception {
+
+			List<Schema.Field> schemaFields = new ArrayList<>();
+			Schema schema = Schema.recordOf("emptyOutput", schemaFields);
+
+			StructuredRecord.Builder builder = StructuredRecord.builder(schema);
+			return builder.build();
+
+		}
+
 	}
 
 	private ThingStreamUtil() {
