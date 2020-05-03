4ba9a1fc39db15b2cb3d85164a0ca2dcefd75107
Predictive Works <info@predictiveworks.eu>
2020-05-03 13:45:01 +0200

    Webhose connector updated

--- src/main/java/de/kp/works/connect/webhose/WebhosePageIterator.java
+++ src/main/java/de/kp/works/connect/webhose/WebhosePageIterator.java
@@ -21,29 +21,138 @@ package de.kp.works.connect.webhose;
 import java.io.Closeable;
 import java.io.IOException;
 import java.util.Iterator;
+import java.util.NoSuchElementException;
 
+import com.google.gson.JsonElement;
+import com.google.gson.JsonObject;
+
+import de.kp.works.connect.core.JsonUtil;
+import de.kp.works.connect.http.HttpResponse;
 import de.kp.works.connect.http.page.HttpPage;
 
 public class WebhosePageIterator implements Iterator<HttpPage>, Closeable {
+	
+	private final WebhoseConfig config;
+	private final WebhoseClient client;
+	
+	private HttpResponse response;
+	/*
+	 * URL management 
+	 */
+	private String nextPageUrl;
+	/*
+	 * PAGE Management
+	 */
+	private boolean currentPageReturned = true;
+	private HttpPage page;
+	
 	/*
 	 * Wehose provides the next url as part of the result
 	 * of the JSON response
 	 */
 	public WebhosePageIterator(WebhoseConfig config) {
+		this.config = config;
+		this.client = new WebhoseClient();		
+		// TODO
+		this.nextPageUrl = null;
+		
+	}
+	
+	private String getNextPageUrl(HttpResponse response) {
+		
+		JsonElement jsonElement = JsonUtil.toJsonElement(response.getBody());
+		if (!jsonElement.isJsonObject())
+			throw new IllegalStateException("Webhose responses must be Json objects.");
+
+		JsonObject jsonObject = jsonElement.getAsJsonObject();
+		WebhoseFormat format = config.getMessageFormat();
+		
+		return WebhoseUtil.getNextUrl(jsonObject, format);
+
 	}
 
+	private boolean hasNextPage() {
+
+		try {
+			/*
+			 * This flag indicates that the request (see next())
+			 * has received a HttpPage and we have to run for
+			 * the next page
+			 */
+			if (currentPageReturned) {
+				
+				page = getNextPage();
+				currentPageReturned = false;
+			
+			}
+			/* check hasNext() to stop on first empty page. */
+			return page != null && page.hasNext();
+		
+		} catch (Exception e) {
+			throw new IllegalStateException("Failed to the load page", e);
+		}
+		
+	}
+	
+	private HttpPage getNextPage() {
+		/*
+		 * This guard works because nextPageUrl has been initialized
+		 * (see above) within the constructor
+		 */
+		if (nextPageUrl == null)
+			return null;
+
+		try {
+
+			if (response != null) 
+				response.close();
+
+			response = new HttpResponse(client.executeHTTP(nextPageUrl));
+
+			if (response.getStatusCode() != 200)
+				throw new Exception("Fetching JSON records from Webhose endpoint failed.");
+			
+		} catch (Exception e) {
+			throw new IllegalStateException("Failed to the load page", e);
+		}
+
+		HttpPage page = new WebhosePage(config, response);
+		nextPageUrl = getNextPageUrl(response);
+
+		return page;
+		
+	}
+	
 	@Override
 	public HttpPage next() {
-		return null;
+
+		if (!hasNextPage()) {
+			throw new NoSuchElementException("No more pages to load.");
+		}
+
+		currentPageReturned = true;
+		return page;
+		
 	}
 
 	@Override
 	public boolean hasNext() {
-		return false;
+		return hasNextPage();
 	}
 
 	@Override
 	public void close() throws IOException {
+		
+		try {
+			if (client != null) {
+				client.close();
+			}
+		} finally {
+			if (response != null) {
+				response.close();
+			}
+		}
+		
 	}
 	
 }
