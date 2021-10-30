b8cb36ba0f2e81d6e25d2df5e572fa3e74975aea
Predictive Works <info@predictiveworks.eu>
2021-10-30 09:32:22 +0200

    Project updated.

--- opencti-connect/src/main/java/de/kp/works/connect/opencti/CTIConfig.java
+++ opencti-connect/src/main/java/de/kp/works/connect/opencti/CTIConfig.java
@@ -18,21 +18,97 @@ package de.kp.works.connect.opencti;
  * 
  */
 
-import de.kp.works.connect.common.BaseConfig;
+import com.google.common.base.Strings;
+import de.kp.works.connect.common.SslConfig;
+import de.kp.works.stream.opencti.CTINames;
+import io.cdap.cdap.api.annotation.Description;
+import io.cdap.cdap.api.annotation.Macro;
 
+import javax.annotation.Nullable;
 import java.util.Properties;
 
-public class CTIConfig extends BaseConfig {
+public class CTIConfig extends SslConfig {
 
-	private static final long serialVersionUID = 3823407192490514926L;
+	@Description("The URL of the OpenCTI server.")
+	@Macro
+	public String serverUrl;
+
+	@Description("The access token to authenticate the OpenCTI user.")
+	@Macro
+	@Nullable
+	public String authToken;
+
+	@Description("The number of threads used to process OpenCTI. Default value is '1'.")
+	@Macro
+	@Nullable
+	public String numThreads;
 
 	public void validate() {
 		super.validate();
 
+		String className = this.getClass().getName();
+
+		if (Strings.isNullOrEmpty(serverUrl)) {
+			throw new IllegalArgumentException(
+					String.format("[%s] The URL of the OpenCTI server must not be empty.", className));
+		}
+
+		if (Strings.isNullOrEmpty(sslKeyStorePath)) {
+			throw new IllegalArgumentException(
+					String.format("[%s] The path to the Keystore must not be empty.", className));
+		}
+
 	}
 
 	public Properties toProperties() {
+
 		Properties props = new Properties();
+		props.setProperty(CTINames.SERVER_URL(), serverUrl);
+
+		if (!Strings.isNullOrEmpty(authToken)) {
+			props.setProperty(CTINames.AUTH_TOKEN(), authToken);
+		}
+
+		if (!Strings.isNullOrEmpty(numThreads)) {
+			props.setProperty(CTINames.NUM_THREADS(), numThreads);
+		}
+
+		/* SSL SUPPORT */
+
+		if (!Strings.isNullOrEmpty(sslCipherSuites)) {
+			props.setProperty(CTINames.SSL_CIPHER_SUITES(), sslCipherSuites);
+		}
+
+		props.setProperty(CTINames.SSL_KEYSTORE_FILE(), sslKeyStorePath);
+
+		if (!Strings.isNullOrEmpty(sslKeyStoreType)) {
+			props.setProperty(CTINames.SSL_KEYSTORE_TYPE(), sslKeyStoreType);
+		}
+
+		if (!Strings.isNullOrEmpty(sslKeyStoreAlgo)) {
+			props.setProperty(CTINames.SSL_KEYSTORE_ALGO(), sslKeyStoreAlgo);
+		}
+
+		if (!Strings.isNullOrEmpty(sslKeyStorePass)) {
+			props.setProperty(CTINames.SSL_KEYSTORE_PASS(), sslKeyStorePass);
+		}
+
+		if (!Strings.isNullOrEmpty(sslTrustStorePath)) {
+			props.setProperty(CTINames.SSL_TRUSTSTORE_FILE(), sslTrustStorePath);
+		}
+
+		if (!Strings.isNullOrEmpty(sslTrustStoreType)) {
+			props.setProperty(CTINames.SSL_TRUSTSTORE_TYPE(), sslTrustStoreType);
+		}
+
+		if (!Strings.isNullOrEmpty(sslTrustStoreAlgo)) {
+			props.setProperty(CTINames.SSL_TRUSTSTORE_ALGO(), sslTrustStoreAlgo);
+		}
+
+		if (!Strings.isNullOrEmpty(sslTrustStorePass)) {
+			props.setProperty(CTINames.SSL_TRUSTSTORE_PASS(), sslTrustStorePass);
+		}
+
 		return props;
 	}
 }
